58f802ed36a8bd1cb4d340483f97ee09841ec677
carmiaS <50803820+carmiaS@users.noreply.github.com>
2021-02-04 10:32:06 -0500

    Connection changes
    Put back server connection code

--- pages/phpcode/connection.php
+++ pages/phpcode/connection.php
@@ -1,9 +1,9 @@
 <?php
 //server database
-$serverName = "localhost";//"35.223.86.91";
-$userName = "root";//"user";
-$password = "Swagger13";//"oakland";
-$dbName = "rtwdb";
+$serverName = "35.223.86.91";
+$userName = "user";
+$password = "oakland";
+$dbName = "rtwdb;
 // Create connection
 $conn = mysqli_connect($serverName, $userName , $password, $dbName);
 ?>
