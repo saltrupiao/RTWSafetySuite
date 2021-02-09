228e37b59e47fafbd886d7479b98071c6003d5e4
saltrupiao <strupiano@oakland.edu>
2021-02-09 10:51:18 -0500

    Pull in values from form into variables, add debug lines to report submitted values in HTML

--- pages/empController.php
+++ pages/empController.php
@@ -8,9 +8,38 @@
     $conn = mysqli_connect($serverName, $userName , $password, $dbName);
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
-        // username and password sent from form
-        $myusername = mysqli_real_escape_string($conn,$_POST['username']);
-        $mypassword = mysqli_real_escape_string($conn,$_POST['password']);
+        // get results from form
+        $fever = mysqli_real_escape_string($conn,$_POST['fever']);
+        $cough = mysqli_real_escape_string($conn,$_POST['cough']);
+        $breath = mysqli_real_escape_string($conn,$_POST['breath']);
+        $soreThroat = mysqli_real_escape_string($conn,$_POST['soreThroat']);
+        $congest = mysqli_real_escape_string($conn,$_POST['congest']);
+        $aches = mysqli_real_escape_string($conn,$_POST['aches']);
+        $tasteSmell = mysqli_real_escape_string($conn,$_POST['tasteSmell']);
+        $headache = mysqli_real_escape_string($conn,$_POST['headache']);
+        $d = mysqli_real_escape_string($conn,$_POST['d']);
+        $nausea = mysqli_real_escape_string($conn,$_POST['nausea']);
+        $covidPositive = mysqli_real_escape_string($conn,$_POST['covidPositive']);
+        $exposure = mysqli_real_escape_string($conn,$_POST['exposure']);
+        $signatureBox = mysqli_real_escape_string($conn,$_POST['signatureBox']);
+
+        // debug statements - values from form
+        echo "<h2>Fever: $fever</h2>";
+        echo "<h2>Cough: $cough</h2>";
+        echo "<h2>Breath: $breath</h2>";
+        echo "<h2>Sore Throat: $soreThroat</h2>";
+        echo "<h2>Congest: $congest</h2>";
+        echo "<h2>Aches: $aches</h2>";
+        echo "<h2>Taste/Smell Loss: $tasteSmell</h2>";
+        echo "<h2>Headache: $headache</h2>";
+        echo "<h2>Dih: $d</h2>";
+        echo "<h2>Nausea: $nausea</h2>";
+        echo "<h2>COVID Positive in last 14 days: $covidPositive</h2>";
+        echo "<h2>Exposed to COVID: $exposure</h2>";
+        echo "<h2>Employee Electronic Signature: $signatureBox</h2>";
+
+
+        /*
         $sql = "SELECT EMP_USERID, EMP_PW, EMP_ISADMIN FROM EMPLOYEE WHERE EMP_USERID = '$myusername' and EMP_PW = '$mypassword'";
         echo "<h1>$sql</h1>";
         $result = mysqli_query($conn,$sql);
@@ -33,4 +62,5 @@
             echo "<h1>$error</h1>";
             echo "<h1>$sql</h1>";
         }
+        */
     }
