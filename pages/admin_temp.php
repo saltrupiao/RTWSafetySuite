53daa3ece2783c228fa4065644585fc2a5cd657d
saltrupiao <strupiano@oakland.edu>
2021-02-21 18:35:12 -0500

    New responsive nav bar v3

--- pages/admin_temp.php
+++ pages/admin_temp.php
@@ -47,7 +47,7 @@ if($row["EMP_ISADMIN"] == "1"){
 <body>
     <section id="nav-bar">
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <nav class="navbar navbar-expand-lg navbar-top topnav" id="myTopnav">
             <h3><a href="admin_temp.php"><b>Return To Work Safety Suite - Admin</b></a></h3>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
