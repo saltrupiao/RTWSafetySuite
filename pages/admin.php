9834325e71005144705cf58c6aa14e2c45638057
saltrupiao <strupiano@oakland.edu>
2021-02-28 17:38:21 -0500

    Add hidden input to override form v2

--- pages/admin.php
+++ pages/admin.php
@@ -129,7 +129,7 @@
                                         echo '<td>'.$nausea.'</td>';
                                         echo '<td>'.$covidPositive.'</td>';
                                         echo '<td>'.$covidExposed.'</td>';
-                                        echo '<td><form action="./controller/adminController.php" method="get"><input type="hidden" value="'.$empID.'"<button type="submit" class="btn btn-danger" value="Submit">OVERRIDE</button></form></td>';
+                                        echo '<td><form action="./controller/adminController.php" method="get"><input type="hidden" value="'.$empID.'"><button type="submit" class="btn btn-danger" value="Submit">OVERRIDE</button></form></td>';
                                         echo "</tr>";
                                     }
                                 } else {
