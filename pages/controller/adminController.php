144a36b2405bb7beeff57dd636647f20041d6c3c
saltrupiao <strupiano@oakland.edu>
2021-02-28 17:43:44 -0500

    Add debug statement to display empID to adminController.php

--- pages/controller/adminController.php
+++ pages/controller/adminController.php
@@ -1,1 +1,13 @@
 <?php
+
+    $serverName = "localhost";
+    $userName = "user";
+    $password = "oakland";
+    $dbName = "rtwdb";
+    // Create connection
+    $conn = mysqli_connect($serverName, $userName , $password, $dbName);
+
+    if($_SERVER["REQUEST_METHOD"] == "GET") {
+        $empID = $_GET['empID'];
+        echo "<h1>".$empID."</h1>";
+    }
