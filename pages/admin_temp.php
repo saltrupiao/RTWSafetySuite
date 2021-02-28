9a0b9868ae924866ee7999ffa5d5a506316c7ae0
saltrupiao <strupiano@oakland.edu>
2021-02-28 15:04:17 -0500

    Add experimental navbar

--- pages/admin_temp.php
+++ pages/admin_temp.php
@@ -60,6 +60,23 @@ $resultTable = $conn->query($sqlTable);
                 <i class="fa fa-bars"></i>
             </a>
         </div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">RTW Safety Suite - Admin</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="#">Employee Stats</a>
+                        <a class="nav-link" href="admin_manage">Manage Employees</a>
+                        <a class="nav-link" href="admin_distance.php">Distance Tracking</a>
+                        <a class="nav-link" href="./controller/logout.php" tabindex="-1">LOGOUT</a>
+                        <a class="nav-link"><?php echo "Welcome" . " " . $adminuser; ?></a>
+                    </div>
+                </div>
+            </div>
+        </nav>
 
     </section>
     <div class="row">
