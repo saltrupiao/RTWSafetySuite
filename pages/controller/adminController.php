00cac57d273b28b74afc58e9686b828839ae1251
saltrupiao <strupiano@oakland.edu>
2021-03-01 22:09:14 -0500

    Add update logic

--- pages/controller/adminController.php
+++ pages/controller/adminController.php
@@ -7,6 +7,8 @@
     // Create connection
     $conn = mysqli_connect($serverName, $userName , $password, $dbName);
 
+    $newStatus = "";
+
     if($_SERVER["REQUEST_METHOD"] == "GET") {
         $empID = $_GET['empID'];
         echo "<h1>".$empID."</h1>";
@@ -16,9 +18,20 @@
 
         while ($rowGetStatus = $resultGetStatus -> fetch_row()) {
             echo "<h1>" . $rowGetStatus[0] . "</h1>";
+            if ($rowGetStatus[0] = "NO") {
+                $newStatus = "YES";
+            } else {
+                $newStatus = "NO";
+            }
+            echo "<h1>" . $newStatus . "</h1>";
         }
 
+        $sqlSwitchStatus = "UPDATE EMPLOYEE SET EMP_STATUS = '$newStatus' WHERE EMPLOYEE.EMP_ID = $empID";
+        if ($conn->query($sqlSwitchStatus) === TRUE) {
+            echo "New record inserted successfully.";
+        } else {
+            echo "Error: " . $sqlSwitchStatus . "<br>" . $conn->error;
+        }
 
-        //$sqlSwitchStatus = "UPDATE EMPLOYEE SET EMP_STATUS = '$status' WHERE EMPLOYEE.EMP_ID = $currentEmpID";
-        //$resultFLName = $conn->query($sqlSwitchStatus);
+        $conn->close();
     }
