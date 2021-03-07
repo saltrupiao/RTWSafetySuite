fee8f3fd9f4f4772e8ff986253bbe59d445e92b2
saltrupiao <strupiano@oakland.edu>
2021-03-07 13:43:02 -0500

    Add head elements in emp.php for styling of symptom reporting form

--- pages/emp.php
+++ pages/emp.php
@@ -42,6 +42,12 @@ $emp_dept = $row2["EMP_DEPT"];
         <link href="../css/main.css" rel="stylesheet" media="all">
         <link rel="stylesheet" href="../css/style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link href="../css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+        <link href="../css/font-awesome.min.css" rel="stylesheet" media="all">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+        <link href="../css/select2.min.css" rel="stylesheet" media="all">
+        <link href="../css/daterangepicker.css" rel="stylesheet" media="all">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <title> Employee Dashboard </title>
         <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
