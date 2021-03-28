2a4d9945248f018a2b54eaab48b33441b18bfce1
carmiaS <50803820+carmiaS@users.noreply.github.com>
2021-03-28 12:07:06 -0400

    Added the signature to table, also added line breaks to headings to test if it minimizes any space between columns

--- pages/admin_symp_history.php
+++ pages/admin_symp_history.php
@@ -66,15 +66,20 @@
                                         <th scope="col">Status</th>
                                         <th scope="col">Fever</th>
                                         <th scope="col">Cough</th>
-                                        <th scope="col">Shortness of Breath</th>
+                                        <th scope="col">Shortness
+                                            <br>of Breath</th>
                                         <th scope="col">Congestion</th>
                                         <th scope="col">Aches</th>
-                                        <th scope="col">Loss of Taste/Smell</th>
+                                        <th scope="col">Loss of
+                                            <br>Taste/Smell</th>
                                         <th scope="col">Headache</th>
                                         <th scope="col">Diarrhea</th>
                                         <th scope="col">Nausea</th>
-                                        <th scope="col">COVID Positive</th>
-                                        <th scope="col">COVID Exposed</th>
+                                        <th scope="col">COVID
+                                            <br>Positive</th>
+                                        <th scope="col">COVID
+                                            <br>Exposed</th>
+                                        <th scope="col">SIGNATURE</th>
                                         <th scope="col">Submit Date</th>
                                     </tr>
                                 </thead>
@@ -106,6 +111,7 @@
                                         $covidPositive = $rowTable['SYMP_COVIDPOS'];
                                         $covidExposed = $rowTable['SYMP_COVIDEXPOS'];
                                         $insertDate = $rowTable['EMP_DATE_INSERT'];
+                                        $empSignature = $rowTable['EMP_SIGNATURE'];
                                         echo "<tr>";
                                         echo '<td>'.$empFullName.'</td>';
                                         echo '<td>'.$status.'</td>';
@@ -120,6 +126,7 @@
                                         echo '<td>'.$nausea.'</td>';
                                         echo '<td>'.$covidPositive.'</td>';
                                         echo '<td>'.$covidExposed.'</td>';
+                                        echo '<td>'.$empSignature.'</td>';
                                         echo '<td>'.$insertDate.'</td>';
                                         echo "</tr>";
                                     }
