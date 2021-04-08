a59dcf7f14e0a8a2c2bd253bcd0a741235b4acbd
saltrupiao <strupiano@oakland.edu>
2021-04-08 14:48:50 -0400

    Add time command for algorithm analysis

--- rtw-capture.py
+++ rtw-capture.py
@@ -8,7 +8,7 @@ import subprocess
 
 def dt():
     currentDateTime = datetime.now()
-    date_fmt = currentDateTime.strftime("%m-%d--%H:%M:%S")
+    date_fmt = currentDateTime.strftime("%m-%d--%H-%M-%S")
     return date_fmt
 
 
@@ -19,17 +19,24 @@ def capture():
         curDateTime = dt()
 
         fn = "capture-" + curDateTime + ".mp4"
+        # Command base source: https://unix.stackexchange.com/questions/58526/trouble-getting-vlc-to-record-from-the-webcam-via-command-line
         vlcCmd1 = 'vlc v4l2:// :input-slave=alsa:// :v4l-vdev="/dev/video0" :v4l-norm=3 :v4l-frequency=-1 :v4l-caching=300 :v4l-chroma="" :v4l-fps=-1.000000 :v4l-samplerate=44100 :v4l-channel=0 :v4l-tuner=-1 :v4l-audio=-1 :v4l-stereo :v4l-width=480 :v4l-height=360 :v4l-brightness=-1 :v4l-colour=-1 :v4l-hue=-1 :v4l-contrast=-1 :no-v4l-mjpeg :v4l-decimation=1 :v4l-quality=100 --sout="#transcode{vcodec=h264,scale=Auto,width=1920,height=1080,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:file{dst=/home/reed/Desktop/social-distance-detector/'
         vlcCmd2 = fn
         vlcCmd3 = ',no-overwrite}"'
         vlcCmdFull = vlcCmd1 + vlcCmd2 + vlcCmd3
         print(vlcCmdFull)
 
-        cmdExecute = subprocess.Popen(vlcCmdFull, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+        recordCmd = subprocess.Popen(vlcCmdFull, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
 
         time.sleep(30)
-        os.killpg(os.getpgid(cmdExecute.pid), signal.SIGTERM)
-        time.sleep(5)
+        os.killpg(os.getpgid(recordCmd.pid), signal.SIGTERM)
+
+        time.sleep(25)
+
+        fnSize = len(fn)
+        detectionCmd = "time python3 social_distance_detector.py --input " + fn + " --output " + fn[:fnSize-4] + ".avi"
+        print("Executing Detection Command:", detectionCmd)
+        os.system(detectionCmd)
 
 
 def main():
