58d3284cad6180e915a6fabf39411d0512536191
saltrupiao <strupiano@oakland.edu>
2021-04-08 11:33:07 -0400

    add vlc command to script

--- rtw-capture.py
+++ rtw-capture.py
@@ -1,6 +1,9 @@
 import threading
 import time
 from datetime import datetime
+import os
+import signal
+import subprocess
 
 
 def dt():
@@ -11,9 +14,22 @@ def dt():
 
 def capture():
     while True:
-        curDateTime = dt()
-        time.sleep(3)
         print("In while loop")
+
+        curDateTime = dt()
+
+        fn = "capture-" + curDateTime + ".mp4"
+        vlcCmd1 = 'vlc v4l2:// :input-slave=alsa:// :v4l-vdev="/dev/video0" :v4l-norm=3 :v4l-frequency=-1 :v4l-caching=300 :v4l-chroma="" :v4l-fps=-1.000000 :v4l-samplerate=44100 :v4l-channel=0 :v4l-tuner=-1 :v4l-audio=-1 :v4l-stereo :v4l-width=480 :v4l-height=360 :v4l-brightness=-1 :v4l-colour=-1 :v4l-hue=-1 :v4l-contrast=-1 :no-v4l-mjpeg :v4l-decimation=1 :v4l-quality=100 --sout="#transcode{vcodec=h264,scale=Auto,width=1920,height=1080,acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:file{dst=/home/reed/Desktop/social-distance-detector/'
+        vlcCmd2 = fn
+        vlcCmd3 = ',no-overwrite}"'
+        vlcCmdFull = vlcCmd1 + vlcCmd2 + vlcCmd3
+        print(vlcCmdFull)
+
+        cmdExecute = subprocess.Popen(vlcCmdFull, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+
+        time.sleep(30)
+        os.killpg(os.getpgid(cmdExecute.pid), signal.SIGTERM)
+        time.sleep(5)
 
 
 def main():
