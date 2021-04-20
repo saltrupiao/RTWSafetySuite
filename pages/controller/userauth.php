194d04dc382c44c36ee4aae947b7a451cc7f9dea
carmiaS <50803820+carmiaS@users.noreply.github.com>
2021-04-20 15:11:44 -0400

    Updated authentication logic for hashed password check

--- pages/controller/userauth.php
+++ pages/controller/userauth.php
@@ -14,29 +14,34 @@
       // username and password sent from form
       $myusername = mysqli_real_escape_string($conn,$_POST['username']);
       $mypassword = mysqli_real_escape_string($conn,$_POST['password']);
-      $sql = "SELECT EMP_USERID, EMP_PW, EMP_ISADMIN FROM EMPLOYEE WHERE EMP_USERID = '$myusername' and EMP_PW = '$mypassword'";
+      $sql = "SELECT EMP_USERID, EMP_PW, EMP_ISADMIN FROM EMPLOYEE WHERE EMP_USERID = '$myusername'";
       echo "<h1>$sql</h1>";
       $result = mysqli_query($conn,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
       $count = mysqli_num_rows($result);
+        
       // If result matched $myusername and $mypassword, table row must be 1 row
-      if($count == 1) {
-         $_SESSION['login_user'] = $myusername;
-          //if isadmin row is 1 then go to admin dashboard
-          if ($row["EMP_ISADMIN"]== "1"){
-              header("location: ../admin.php");
-          }
-          //if regular user go to emp dashboard
-          else {
-              header("location: ../emp.php");
-          }
-      }else {
-         $error = "Your Login Name or Password is invalid";
-         $_SESSION["error"] = $error;
-         header("location: ../login.php");
-         //echo "<h1>$error</h1>";
-         //echo "<h1>$sql</h1>";
-      }
+            if($count == 1) {
+                //checking password after unhashing
+                if (password_verify($mypassword, $row['EMP_PW']) == TRUE) {
+                    $_SESSION['login_user'] = $myusername;
+                    //if isadmin row is 1 then go to admin dashboard
+                    if ($row["EMP_ISADMIN"]== "1"){
+                        header("location: ../admin.php");
+                    }
+                    //if regular user go to emp dashboard
+                    else {
+                        header("location: ../emp.php");
+                    }
+                } else {
+                    $error = "Your Login Name or Password is invalid";
+                    $_SESSION["error"] = $error;
+                    header("location: ../login.php");
+                    //echo "<h1>$error</h1>";
+                    //echo "<h1>$sql</h1>";
+                }
+        }
+      
    }
 ?>
