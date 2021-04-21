b60e0d8f3c3bc1c3c81ba202718b24e324321d63
carmiaS <50803820+carmiaS@users.noreply.github.com>
2021-04-21 15:14:22 -0400

    Added code to log error

--- pages/controller/userauth.php
+++ pages/controller/userauth.php
@@ -10,13 +10,17 @@
     $dbName = "rtwdb";
     // Create connection
     $conn = mysqli_connect($serverName, $userName , $password, $dbName);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        echo '<script> console.log("Connection to db failed");</script>';
+    }
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form
       $myusername = mysqli_real_escape_string($conn,$_POST['username']);
       $mypassword = mysqli_real_escape_string($conn,$_POST['password']);
       $sql = "SELECT EMP_USERID, EMP_PW, EMP_ISADMIN FROM EMPLOYEE WHERE EMP_USERID = '$myusername'";
       echo "<h1>$sql</h1>";
-      $result = mysqli_query($conn,$sql) or die(mysqli_error());
+      $result = mysqli_query($conn,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
       $count = mysqli_num_rows($result);
